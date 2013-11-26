3b45991ed8d271d2216848c94767c7b5f8f75e0b
alpayOnal <alpaycom@gmail.com>
2013-11-26 17:58:36 +0200

    Request işlemleri guzzle servisine geçirildi.

--- router.php
+++ router.php
@@ -1,75 +1,78 @@
 <?php
-class quickCurl
-{
-	public static function run($url,$path,$reqtype="POST",
-		$httpv="HTTP/1.1",$headers=false,$data=null){ 
-		
-		if (!function_exists('curl_init')){
-			die('Curl is not installed!');
-		}
- 
-		$ch = curl_init(); 
-        	curl_setopt($ch, CURLOPT_URL,$url.'/'.$path);
-                
-		$headerst = array($reqtype." ".$path." ".$httpv); 
-		
-		if ($headers)
-			foreach($headers as $k) $headerst[]=$k;
-		
-		if ($data){
-			$headerst[]="Content-length: ".strlen($data);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-		}
-		
-      		curl_setopt($ch, CURLOPT_HTTPHEADER, $headerst);
-      		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-      		curl_setopt($ch, CURLOPT_TIMEOUT, 30); 
-		
-		$result = curl_exec($ch); 		
-		curl_close($ch); 		
-	 
-		return $result;
-	}
-}
+require 'vendor/autoload.php';
+error_reporting(E_ALL);
+ini_set("display_errors", 1); 
 
-$r=$_REQUEST;
+$r = $_REQUEST;
 
-$patht=explode('/',$r['path']);
-$path='';
+$patht = explode('/',$r['path']);
+$path = '';
 if (isset($patht))
 	foreach($patht as $p){
-		 $path.='/'.rawurlencode($p);
+		 $path.= '/'.rawurlencode($p);
 	}
 
-$version=$r['version'];
-$method=$r['method'];
+$path = mb_substr($path,1);
+$version = $r['version'];
+$method = $r['method'];
 
 if (isset($r['headerFieldNs'])){
 	
-	$headers=array();
+	$headers = array();
 	
 	foreach($r['headerFieldNs'] as $i=>$n){
 		if($n=='host')
-			$host=$r['headerFieldVs'][$i];
+			$host = $r['headerFieldVs'][$i];
 					
-		$headers[]=$n.':'.$r['headerFieldVs'][$i];
+		$headers[] = $n.':'.$r['headerFieldVs'][$i];
 		
-		$dataFields[$r['headerFieldNs'][$i]]=$r['headerFieldVs'][$i];
+		$dataFields[$r['headerFieldNs'][$i]] = $r['headerFieldVs'][$i];
 	}	
 }
 
 
 if (isset($r['postFieldNs'])){
 	
-	$postFields=array();
-	
-	foreach($r['postFieldNs'] as $i=>$n)
-		$postFields[$r['postFieldNs'][$i]]=$r['postFieldVs'][$i];
-	
-	$postdata = http_build_query($postFields);
-	
-}else $postdata=null;
+	$postFields = array();
+	$pname = '';
+	foreach($r['postFieldNs'] as $i=>$n){
+		
+		if (strstr($n,'[]')){
 
-echo quickCurl::run($host,$path,$method,$version,$headers,$postdata);
+			if ($pname!=str_replace('[]','',$n))
+				$pcount = 0;
 
+			$pname = str_replace('[]','',$n);
+			$r['postFieldNs'][$i] = $pname.'['.$pcount.']';
+			$pcount++;
+		}else
+			$pcount =0;
+
+		$postFields[$r['postFieldNs'][$i]] = $r['postFieldVs'][$i];
+	}
+}
+
+use Guzzle\Http\Client;
+use Guzzle\Http\Message;
+use Guzzle\Http\Message\RequestFactory;
+
+$client = new Client();
+$requestFactory = new RequestFactory();
+$request = $requestFactory->fromMessage(
+	$method .' '.$path.' '.$version."\r\n" .
+	"Host: ".$host." \r\n\r\n"
+);
+
+// method kontrolü yapılmalı. GET HEAD TRACE OPTIONS 
+if (isset($postFields))
+	$client->addPostFields($postFields);
+
+if (isset($dataFields['host']))
+	unset($dataFields['host']);
+
+$request->addHeaders($dataFields);
+$request->setClient($client);
+$response = $request->send();
+
+echo $response->getBody();
 ?>

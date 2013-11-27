4d0ecea0b361083a6fe5ac9400eaead5ec250f2c
alpayOnal <alpaycom@gmail.com>
2013-11-27 02:15:41 +0200

    router script düzenlendi

--- router.php
+++ router.php
@@ -1,78 +1,126 @@
 <?php
 require 'vendor/autoload.php';
-error_reporting(E_ALL);
-ini_set("display_errors", 1); 
-
-$r = $_REQUEST;
-
-$patht = explode('/',$r['path']);
-$path = '';
-if (isset($patht))
-	foreach($patht as $p){
-		 $path.= '/'.rawurlencode($p);
-	}
-
-$path = mb_substr($path,1);
-$version = $r['version'];
-$method = $r['method'];
-
-if (isset($r['headerFieldNs'])){
-	
-	$headers = array();
-	
-	foreach($r['headerFieldNs'] as $i=>$n){
-		if($n=='host')
-			$host = $r['headerFieldVs'][$i];
-					
-		$headers[] = $n.':'.$r['headerFieldVs'][$i];
-		
-		$dataFields[$r['headerFieldNs'][$i]] = $r['headerFieldVs'][$i];
-	}	
-}
-
-
-if (isset($r['postFieldNs'])){
-	
-	$postFields = array();
-	$pname = '';
-	foreach($r['postFieldNs'] as $i=>$n){
-		
-		if (strstr($n,'[]')){
-
-			if ($pname!=str_replace('[]','',$n))
-				$pcount = 0;
-
-			$pname = str_replace('[]','',$n);
-			$r['postFieldNs'][$i] = $pname.'['.$pcount.']';
-			$pcount++;
-		}else
-			$pcount =0;
-
-		$postFields[$r['postFieldNs'][$i]] = $r['postFieldVs'][$i];
-	}
-}
 
 use Guzzle\Http\Client;
 use Guzzle\Http\Message;
 use Guzzle\Http\Message\RequestFactory;
 
-$client = new Client();
-$requestFactory = new RequestFactory();
-$request = $requestFactory->fromMessage(
-	$method .' '.$path.' '.$version."\r\n" .
-	"Host: ".$host." \r\n\r\n"
-);
+class APIRouter
+{
 
-// method kontrolü yapılmalı. GET HEAD TRACE OPTIONS 
-if (isset($postFields))
-	$client->addPostFields($postFields);
+	public $host;
 
-if (isset($dataFields['host']))
-	unset($dataFields['host']);
+	public $path;
 
-$request->addHeaders($dataFields);
-$request->setClient($client);
-$response = $request->send();
+	public $method;
 
-echo $response->getBody();
-?>
+	public $version;
+
+	public $headers;
+
+	public $postFields;
+
+	public function __construct()
+	{
+		error_reporting(E_ALL);
+		ini_set("display_errors", 1); 
+
+		$this->r = $_REQUEST;
+		
+		$this->createParams();
+		$this->version = $r['version']; //http version
+		$this->method = $r['method']; 
+	}
+
+	public function createParams()
+	{
+		$this->path = $this->createPath();
+		$this->headers = $this->createHeaders();
+		$this->postFields = $this->createPostFields();
+	}
+	
+	public function createPath()
+	{
+		$patht = explode('/',$this->r['path']);
+		$path = '';
+		if (isset($patht)){
+			foreach($patht as $p){
+				 $path.= '/'.rawurlencode($p);
+			}
+		}
+
+		return mb_substr($path,1);
+	}
+
+	public  function createHeaders(){
+
+		if (isset($this->r['headerFieldNs'])){
+			
+			$r = $this->r;	
+			$headers = array();
+			
+			foreach($r['headerFieldNs'] as $i=>$n){
+				if($n=='host')
+					$this->host = $r['headerFieldVs'][$i];
+							
+				$headers[$r['headerFieldNs'][$i]] = $r['headerFieldVs'][$i];
+
+				return $headers;
+			}	
+		}
+
+		return false;
+	}
+	
+	public function createPostFields()
+	{
+		if (isset($this->r['postFieldNs'])){
+			
+			$postFields = array();
+			$pname = '';
+			foreach($r['postFieldNs'] as $i=>$n){
+				
+				if (strstr($n,'[]')){
+
+					if ($pname!=str_replace('[]','',$n))
+						$pcount = 0;
+
+					$pname = str_replace('[]','',$n);
+					$r['postFieldNs'][$i] = $pname.'['.$pcount.']';
+					$pcount++;
+				}else
+					$pcount = 0;
+
+				$postFields[$r['postFieldNs'][$i]] = $r['postFieldVs'][$i];
+
+				return $postFields;
+			}
+		}
+
+		return false;
+	}
+
+	public function run()
+	{
+		$client = new Client();
+		$requestFactory = new RequestFactory();
+		$request = $requestFactory->fromMessage(
+			$this->method .' '.$this->path.' '.$this->version."\r\n" .
+			"Host: ".$this->host." \r\n\r\n"
+		);
+
+		if (isset($this->postFields) && !in_array($this->method,array('GET','HEAD','TRACE','OPTIONS')))
+			$client->addPostFields($this->postFields);
+
+		if (isset($this->headers['host']))
+			unset($this->headers['host']);
+
+		$request->addHeaders($this->headers);
+		$request->setClient($client);
+		$response = $request->send();
+
+		echo $response->getBody();
+	}
+
+$router = new APIRouter();
+$router->run();
